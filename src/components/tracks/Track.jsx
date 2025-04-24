8d18635b2f3028a619f0ee2daebadceffcdb3318
webQbe <webcubesolutionslk@gmail.com>
2025-04-24 15:05:49 +0530

    Displayed track & album titles and added View Lyrics button

--- src/components/tracks/Track.jsx
+++ src/components/tracks/Track.jsx
@@ -1,10 +1,11 @@
 /* Show Individual Track Info */
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Track = ({ track }) => { // Pass track prop from .map() in Tracks.jsx
   
   // Destructure artist
-  const { artist } = track
+  const { artist, title, album, id } = track
 
   // console.log("Track object:", track); 
   return (
@@ -17,6 +18,16 @@ const Track = ({ track }) => { // Pass track prop from .map() in Tracks.jsx
                         artist?.name 
                      }
                 </h5> 
+                {/* Display Track Title & Album Title with Font Awesome icons */}
+                <p className="card-text">
+                  <strong><i className="fas fa-play"></i>Track</strong>: { title }
+                  <br />
+                  <strong><i className="fas fa-compact-disc"></i>Album</strong>: { album?.title }
+                </p>
+                {/* Route to lyrics page. */}
+                <Link to={`lyrics/track/${id}`} className="btn btn-dark btn-dark btn-block">
+                     <i className="fas fa-chevron-right"></i> View Lyrics
+                </Link>
             </div>
         </div>
     </div>
