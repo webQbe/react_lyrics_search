871bb62319ab42de6e5f2e448f479472314bea76
webQbe <webcubesolutionslk@gmail.com>
2025-04-27 23:17:50 +0530

    Created search form

--- src/components/tracks/Search.jsx
+++ src/components/tracks/Search.jsx
@@ -4,7 +4,7 @@ import { Consumer } from '../../context' // To access global state
 
 const Search = () => {
 
-    const [ trackTitle, setTrackTitle ] = useState() // To store search input value
+    const [ trackTitle, setTrackTitle ] = useState('') // To store search input value (trackTitle starts empty)
 
   return (
     <Consumer>
@@ -24,6 +24,28 @@ const Search = () => {
 
                         {/*  Smaller centered paragraph */}
                         <p className="lead text-center">Get the lyrics for any song</p>
+
+                        {/* Search Form */}   
+                        <form onSubmit={findTrack}>                 
+                            <div className="form-group">
+                                {/* Search text input */}
+                                <input 
+                                    type="text" 
+                                    className="form-control form-control lg" // big input
+                                    placeholder="Song title..."
+                                    name="trackTitle"
+                                    value={ trackTitle }
+                                    onChange={
+                                         // Update local state trackTitle whenever user types
+                                        (e) => setTrackTitle(e.target.value)
+                                    }
+                                />
+                            </div>
+                            {/* Blue color large submit button */}
+                            <button type='submit' className="btn btn-primary btn-lg btn-block mb-5">
+                               Get Track Lyrics         
+                            </button>
+                        </form>
                     </div>
                 )
         }}
